7c0c9b44e3205945b8ab18e7bb9590d27570acba
Robbie <akotoro22@gmail.com>
2018-06-08 17:14:04 +0000

    updates were made to info section

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,6 @@
 //Naming the Cahced files
 const cacheName = 'pwa-starter-kit';
-const cacheVersion = `${cacheName}::1.3.0`;
+const cacheVersion = `${cacheName}::1.3.2`;
 
 //Variable to hold cahce files
 const cachedFiles = [
