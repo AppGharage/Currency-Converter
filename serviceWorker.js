73492c36f2d7f079f1a39f71d9f197db6508131f
Robbie <akotoro22@gmail.com>
2018-06-08 14:50:59 +0000

    serviceworker cache updated

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,6 @@
 //Naming the Cahced files
 const cacheName = 'pwa-starter-kit';
-const cacheVersion = `${cacheName}::1.2.2`;
+const cacheVersion = `${cacheName}::1.3.0`;
 
 //Variable to hold cahce files
 const cachedFiles = [
@@ -9,15 +9,14 @@ const cachedFiles = [
 ];
 
 //Variables to hold Network files
-const networkFiles = [
-];
+const networkFiles = [];
 
 //Installing Service Worker
 self.addEventListener('install', event => {
     console.log('[pwa install]');
     event.waitUntil(
         caches.open(cacheVersion)
-            .then(cache => cache.addAll(cachedFiles))
+        .then(cache => cache.addAll(cachedFiles))
     );
 });
 
@@ -28,7 +27,7 @@ self.addEventListener('activate', event => {
         caches.keys().then(keys =>
             Promise.all(
                 keys.filter(key => key.indexOf(cacheName) === 0 && key !== cacheVersion)
-                    .map(key => caches.delete(key))
+                .map(key => caches.delete(key))
             )
         )
     );
@@ -41,16 +40,16 @@ self.addEventListener('fetch', event => {
         console.log('[network fetch]', event.request.url);
         event.respondWith(
             caches.match(event.request)
-                .then(response => response || fetch(event.request))
+            .then(response => response || fetch(event.request))
         );
     } else {
         console.log('[pwa fetch]', event.request.url);
         event.respondWith(
             caches.match(event.request)
-                .then(response => {
-                    caches.open(cacheVersion).then(cache => cache.add(event.request.url));
-                    return response || fetch(event.request);
-                })
+            .then(response => {
+                caches.open(cacheVersion).then(cache => cache.add(event.request.url));
+                return response || fetch(event.request);
+            })
         );
     }
 });
